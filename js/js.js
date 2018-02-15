2b117762ea9ebb0299e304ee07a7a8d9c7997609
DJ2Play <32370146+DJ2Play@users.noreply.github.com>
2018-02-15 20:02:42 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,17 @@
+ //LEER XML de xml/preguntas.xml
+ var xhttp = new XMLHttpRequest();
+ xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+   gestionarXml(this);
+  }
+ };
+ xhttp.open("GET", "XML.xml", true);
+ xhttp.send();
+}
+
+//****************************************************************************************************
+// Recuperamos los datos del fichero XML xml/preguntas.xml
+// xmlDOC es el documento leido XML. 
+function gestionarXml(dadesXml){
+ var xmlDoc = dadesXml.responseXML; //Parse XML to xmlDoc
+}
